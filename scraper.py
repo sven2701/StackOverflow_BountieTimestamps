30e19e1d1cfd7ce61c08f666db8e80c5651968f6
svenp <.pruess2701@googlemail.com>
2025-01-27 17:50:20 +0100

    added first working scraper

--- scraper.py
+++ scraper.py
@@ -84,11 +84,11 @@ def configure_driver():
     """
     chrome_options = ChromeOptions()
     # Uncomment the following options for headless mode and faster performance
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--blink-settings=imagesEnabled=false")
 
     driver = uc.Chrome(options=chrome_options)
     driver.set_page_load_timeout(SELENIUM_TIMEOUT)
